670068f2631f291ad8d58454350f01c268914f4a
unknown <kamaljain2500@gmail.com>
2022-03-16 18:33:07 +0530

    added menu page

--- src/app/pages/menupage/menupage.component.ts
+++ src/app/pages/menupage/menupage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-menupage',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenupageComponent implements OnInit {
 
-  constructor() { }
+  menuId: any;
+  menuData: any;
+
+  constructor(private param: ActivatedRoute, private service: OrderDetailsService) { }
 
   ngOnInit(): void {
+    this.menuId = this.param.snapshot.paramMap.get('id');
+    if(this.menuId){
+        this.menuData = this.service.foodDetails.filter((value)=>{
+          return value.id = this.menuId;
+        })
+    }
   }
 
 }
